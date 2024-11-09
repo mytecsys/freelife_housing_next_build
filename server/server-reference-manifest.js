101261e42b06ecf5a4e7ed14c7c6afca24cc90df
SohelSheikh7245 <s.sohel@mytecsys.in>
2024-11-09 20:46:37 +0530

    locality changes in progress

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"V0URjVsQiun6DmUYMdpWxDa/kXeyxXoIeWIoItGVWIU=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"VTHCzsnxnaILe5a15RBCB/pd30pN1v6ZjKas+J0jMuA=\"}"
