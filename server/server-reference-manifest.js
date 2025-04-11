2f8f05436e84e04d11d8d3ef6e7355c989fd8537
SohelSheikh7245 <s.sohel@mytecsys.in>
2025-04-11 19:57:56 +0530

    add pdf in floorplan

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"n3hQQnxzGnfYv9sn5BOCRpzxS2RzNeI5fw10bjdTfUc=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"o60+OPWVK2XgjJ96X2B7b/1V8ColpBTmeMstXYD8FjM=\"}"
